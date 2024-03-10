7f3ef6469baac9dee045be1e4157805893eb9fbb
Ademir <65183999+AdemirRed@users.noreply.github.com>
2024-03-10 20:47:23 -0300

    melhorias no site

--- script.js
+++ script.js
@@ -27,13 +27,16 @@ function closeVideoPopup() {
 closeVideoPopup()
 
 
-
-// Script para desativar elementos após 5 segundos de inatividade
+// Script para desativar elementos após 10 segundos de inatividade
 let idleTime = 0; // Tempo de inatividade em segundos
-const elementosDesativar = ["background-image", "cabecalho", "welcome-message"]; // IDs dos elementos CSS
+const classesDesativar = ["background-image", "cabecalho", "welcome-message"]; // Classes dos elementos CSS
 
 document.addEventListener("mousemove", () => {
   idleTime = 0; // Reinicia o tempo de inatividade a cada movimento do mouse
+
+  // Muta novamente o vídeo
+  const video = document.querySelector('.caixa-video video');
+  video.muted = true;
 });
 
 document.addEventListener("keydown", () => {
@@ -43,6 +46,28 @@ document.addEventListener("keydown", () => {
 setInterval(() => {
   idleTime++; // Incrementa o tempo de inatividade a cada segundo
 
-  if (idleTime >= 5) { // Desativa elementos após 5 segundos de inatividade
-    elementosDesativar.forEach((id) => {
-      document.getElementById(id).style.pointerEvents = "none"; // Desativa a interação com o elemento
+  if (idleTime >= 3) { // Desativa elementos após 10 segundos de inatividade
+    classesDesativar.forEach((className) => {
+      const elements = document.getElementsByClassName(className);
+      Array.from(elements).forEach((element) => {
+        element.style.pointerEvents = "none"; // Desativa a interação com o elemento
+        element.style.opacity = "0.0"; // Diminui a opacidade do elemento
+        element.style.transition = "opacity 1s"; // Adiciona uma transição suave de 1 segundo
+      });
+    });
+
+    // Ativar o som do vídeo
+    const video = document.querySelector('.caixa-video video');
+    video.muted = false;
+  } else { // Reativa elementos se houver atividade
+    classesDesativar.forEach((className) => {
+      const elements = document.getElementsByClassName(className);
+      Array.from(elements).forEach((element) => {
+        element.style.pointerEvents = "auto"; // Reativa a interação com o elemento
+        element.style.opacity = "1"; // Restaura a opacidade do elemento
+        element.style.transition = "opacity 1s"; // Adiciona uma transição suave de 1 segundo
+      });
+    });
+  }
+}, 1000); // Verifica a cada segundo
+
